d70fb0f242ffe7d1c3f041c25b0e9f133c44650f
johanny-M <95614898+johanny-M@users.noreply.github.com>
2024-09-23 02:26:22 -0400

    update the new file on the repo

--- redux/reducer.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "@reduxjs/toolkit";
-import { fakeApi } from "./api";
-
-
-
-export const rootReducer = combineReducers({
-                [fakeApi.reducerPath]: fakeApi.reducer
-})
